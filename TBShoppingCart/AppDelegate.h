0a32e10cc25a9ee658019dab283d2545f51bac00
baiyidjp <tztddong@dongjiangpengdemac-mini.local>
2016-04-27 10:30:02 +0800

    1.0

---
+++ TBShoppingCart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TBShoppingCart
+//
+//  Created by tztddong on 16/4/27.
+//  Copyright © 2016年 dongjiangpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
